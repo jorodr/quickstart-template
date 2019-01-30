39ea5bd249756e59758c703364b2d7d13f3ff1a3
user <user@DESKTOP-56G028R>
2019-01-30 13:24:44 +0200

    Full template in the fist stage

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8" />  
+      <link type="text/css" rel="stylesheet" href="css/styles.css" />
+    </head>
+    <body>
+        <header>
+            <div class="header-top-row">
+                <div class="logo">
+                    <div class="logo-image">
+                      <img src="images/logo-image.png" />
+                    </div>
+                    <div class="logo-text">
+                        <h3>YOURNAME</h3>
+                    </div>
+                </div><!-- /logo -->
+                <div class="menu">
+                    <nav>
+                        <ul class="main-navigation">
+                            <li><a href="#" >home</a></li>
+                            <li><a href="#" >about us</a></li>
+                            <li><a href="#" >info</a></li>
+                            <li><a href="#" >contact</a></li>
+                        </ul>
+                    </nav>
+                </div><!-- /menu -->
+            </div><!-- /header-top-row -->
+            
+            <div class="header-bottom-row">
+                <div class="header-bottom-left">
+                    <div class="header-bottom-left-content">
+                      <h3>teamwork</h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean posuere, risus sit amet maximus ornare, odio mi consectetur ligula, non tristique felis elit dignissim odio. Phasellus posuere euismod fermentum. Integer quis lectus non quam semper consequat. Sed volutpat libero enim, id pellentesque quam sagittis a. Vestibulum porttitor tellus et consequat. 
+                      </p>
+                      <a href="#">Read more</a>
+                    </div>                    
+                </div><!-- /header-bottom-left -->
+                <div class="header-bottom-right">
+                  <img src="images/imac.png" />
+                </div><!-- /header-bottom-right -->
+            </div>
+        </header>
+        <main>
+            <div class="tripple">
+                <div class="tripple-item tripple-left">
+                  <img class="tripple-image" src="images/asset_1.svg" />
+                  <h3>Custom Design</h3>
+                  <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra dictum magna, sed gravida velit rutrum vel. Curabitur quis dui lobortis odio blandit varius et sit amet diam. Aliquam rutrum elementum nisl, ut viverra sem facilisis consectetur. Donec blandit auctor. 
+                  </p>
+                </div><!-- /tripple-left -->
+                <div class="tripple-item tripple-middle">
+                  <img class="tripple-image" src="images/asset_2.svg" />
+                  <h3>Custom Design</h3>
+                  <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra dictum magna, sed gravida velit rutrum vel. Curabitur quis dui lobortis odio blandit varius et sit amet diam. Aliquam rutrum elementum nisl, ut viverra sem facilisis consectetur. Donec blandit auctor. 
+                  </p>
+                </div><!-- /tripple-middle -->
+                <div class="tripple-item tripple-right">
+                  <img class="tripple-image" src="images/asset_3.svg" />
+                  <h3>Custom Design</h3>
+                  <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra dictum magna, sed gravida velit rutrum vel. Curabitur quis dui lobortis odio blandit varius et sit amet diam. Aliquam rutrum elementum nisl, ut viverra sem facilisis consectetur. Donec blandit auctor. 
+                  </p>
+                </div><!-- /tripple-right -->
+            </div><!-- /tripple -->
+            
+            <div class="top-page page-content">
+                <div class="post-content">
+                    <h3>Content Management</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec aliquet elit. Integer eu neque vel ligula bibendum pretium molestie eget nisl. Nam ac purus tristique, sodales urna at, condimentum magna. Proin hendrerit sem eget euismod ultricies. Duis luctus in nisi et ultricies. Nullam fermentum, nisl ut eleifend porttitor, purus dolor bibendum massa, eget mollis urna lectus pretium dolor. Cras ullamcorper sodales auctor.
+                    </p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="post-thumbnail">
+                    <img src="images/speed acces.svg" alt="Speed Access" />
+                </div>
+            </div>
+            <div class="bottom-page page-content">
+                <div class="post-content">                    
+                    <h3>Our Team</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec aliquet elit. Integer eu neque vel ligula bibendum pretium molestie eget nisl. Nam ac purus tristique, sodales urna at, condimentum magna. Proin hendrerit sem eget euismod ultricies. Duis luctus in nisi et ultricies. Nullam fermentum, nisl ut eleifend porttitor, purus dolor bibendum massa, eget mollis urna lectus pretium dolor. Cras ullamcorper sodales auctor.
+                    </p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="post-thumbnail">
+                    <img src="images/workplace-1245776.jpg" alt="Work Place" />
+                </div>
+            </div>            
+            
+            <div class="partners-section">
+                <h2>Our Partners</h2>
+                <div class="partners">
+
+                    <div class="partner">
+                        <img src="images/logo_partner_1.svg"alt="Partner 1" />
+                    </div>
+                    <div class="partner">
+                        <img src="images/logo_partner_2.svg"alt="Partner 1" />
+                    </div>
+                    <div class="partner">
+                        <img src="images/logo_partner_3.svg"alt="Partner 1" />
+                    </div>
+                    <div class="partner">
+                        <img src="images/logo_partner_4.svg"alt="Partner 1" />
+                    </div>
+                </div>
+            </div><!-- partners-section -->            
+            
+            <div class="our-team">
+                <div class="post-content">                    
+                    <h3>Our Team</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec aliquet elit. Integer eu neque vel ligula bibendum pretium molestie eget nisl. Nam ac purus tristique, sodales urna at, condimentum magna. Proin hendrerit sem eget euismod ultricies. Duis luctus in nisi et ultricies. Nullam fermentum, nisl ut eleifend porttitor, purus dolor bibendum massa, eget mollis urna lectus pretium dolor. Cras ullamcorper sodales auctor.
+                    </p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="post-thumbnail">
+                    <img src="images/notification massage_poster_u846.png" alt="Work Place" />
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div class="footer-top">
+                <div class="footer-logo">
+                    <img src="images/logo-image.png" />
+                </div>
+                <div class="footer-company">
+                    <h3>Your Company</h3>
+                </div>
+            </div>
+            
+            <div class="footer-middle">
+                <div class="footer-middle-left">
+                    <h3>Address</h3>
+                    <p>                    
+                        Address: Lorem Ipsum<br />
+                        Mobile: 0000000000<br />
+                        Fax: 0000000000<br />
+                        Mail: office@yourcompany.com<br />
+                        www.yourcompany.com
+                    </p>  
+                </div>
+                <div class="footer-middle-middle">
+                    <h3>Contact us</h3>
+                </div>
+                <div class="footer-middle-right">
+                    <h3>Social Media</h3>
+                    <ul class="footer-social-media">
+                        <li><a href="#" class="instagram-icon"><img src="images/instagram.svg" alt="Instagram icon"/></a></li>
+                        <li><a href="#" class="facebook-icon"><img src="images/facebook.svg"  alt="Facebook icon"/></a></li>
+                        <li><a href="#" class="linkedin-icon"><img src="images/asset-14.svg"  alt="Facebook icon"/></a></li>
+                        <li><a href="#" class="tweeter-icon"><img src="images/twitter.svg"  alt="Tweeter icon"/></a></li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="footer-bottom">
+                <div class="footer-menu">
+                    <nav>
+                        <ul class="footer-navigation">
+                            <li><a href="#" >home</a></li>
+                            <li><a href="#" >about us</a></li>
+                            <li><a href="#" >info</a></li>
+                            <li><a href="#" >contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="copyright">
+                &copy; Design by JenyDesign, created by SofiaSite;
+            </div>
+        </footer>
+    </body>
+</html>
